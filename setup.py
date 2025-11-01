c1984d954e806f875cac805e2a1142fae76c3629
Aditya Kumar Pandey <adityapande107@gmail.com>
2025-11-01 19:02:50 +0000

    folder structure added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI Project",
+    version="0.0.0",
+    package=find_packages(),
+    install_requires=[]
+)
--- template.py
+++ template.py
@@ -23,10 +23,10 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"creating directory; {filedir} for the file {filename}")
 
-        if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-            with open(filepath, "w") as f:
-                pass
-                logging.info(f"creating empty file: {filepath}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
 
-        else:
-            logging.info(f"{filename} already exists")
+    else:
+        logging.info(f"{filename} already exists")
